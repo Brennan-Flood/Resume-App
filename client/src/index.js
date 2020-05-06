22bd251958e642177877a72b547e95ef3475e2e9
Brennan <flood.brennan@gmail.com>
2020-05-06 16:53:02 -0400

    update frontend index.js

--- client/src/index.js
+++ client/src/index.js
@@ -12,12 +12,81 @@ import { ApolloLink } from "apollo-link";
 import Mutations from "./graphql/mutations";
 const { VERIFY_USER } = Mutations;
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+let cache;
+let client;
+let currentUserId;
+
+async function setupClient() {
+  cache = new InMemoryCache({
+    dataIdFromObject: object => object._id || null
+  });
+
+  const httpLink = createHttpLink({
+    uri: "http://localhost:5000/graphql",
+    headers: {
+      authorization: localStorage.getItem("auth-token")
+    }
+  });
+
+  const errorLink = onError(({ graphQLErrors }) => {
+    // if (graphQLErrors) graphQLErrors.map(({ message }) => console.log(message));
+    if (graphQLErrors) {
+      graphQLErrors.map(({ message }) => console.log(message));
+    } else {
+      window.location.reload();
+    }
+  });
+
+  client = new ApolloClient({
+    link: ApolloLink.from([errorLink, httpLink]),
+    cache,
+    onError: ({ networkError, graphQLErrors }) => {
+      console.log("graphQLErrors", graphQLErrors);
+      console.log("networkError", networkError);
+    }
+  });
+}
+
+async function populateCache() {
+  const token = localStorage.getItem("auth-token");
+
+  await cache.writeData({
+    data: {
+      isLoggedIn: Boolean(token)
+    }
+  });
+
+  if (token) {
+    await client
+      .mutate({ mutation: VERIFY_USER, variables: { token } })
+      .then(({ data }) => {
+        currentUserId = data.verifyUser._id
+
+        cache.writeData({
+          data: {
+            isLoggedIn: data.verifyUser.loggedIn,
+            currentUserId: data.verifyUser._id
+          }
+        });
+      });
+  }
+}
+
+setupClient()
+  .then(() => populateCache())
+  .then(() => {
+    const Root = () => {
+      return (
+        <ApolloProvider client={client}>
+          <HashRouter>
+            <App currentUserId={currentUserId} />
+          </HashRouter>
+        </ApolloProvider>
+      );
+    };
+
+    ReactDOM.render(<Root />, document.getElementById("root"));
+  })
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
