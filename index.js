4a0e36bf3c4337e4f4f65bca5469e2d47a3ca1f9
Brennan <flood.brennan@gmail.com>
2020-05-19 01:11:08 -0400

    add test for ec2

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 const app = require("./server/server");
-const express = require("express");
+// const express = require("express");
 const port = process.env.PORT || 5000;
 
 app.listen(port, () => {
   console.log(`Server listening on port ${port}`);
 });
 
-app.get("/", (req, res) => {
-  app.use(express.static('client'));
-})
+// app.get("/", (req, res) => {
+//   app.use(express.static('client'));
+// })
