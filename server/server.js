d8b1d44e3c7d49ae2ff3ba01b6c51fc70655fd34
Brennan <flood.brennan@gmail.com>
2020-05-18 22:54:46 -0400

    add production line

--- server/server.js
+++ server/server.js
@@ -9,13 +9,13 @@ const path = require('path');
 const schema = require("./schema/schema");
 
 
-// if (process.env.NODE_ENV === 'production') {
-//   app.use(express.static('client/build'));
-//   app.get('/', (req, res) => {
-//     res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-//   })
-// { useNewUrlParser: true }
-// };
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('client/build'));
+  app.get('/', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  })
+{ useNewUrlParser: true }
+};
 
 mongoose
   .connect(db, { useUnifiedTopology: true, useNewUrlParser: true })
