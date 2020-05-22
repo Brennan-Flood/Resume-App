60e4ff6c7819afa486b4925056887887309ef364
Brennan <flood.brennan@gmail.com>
2020-05-22 05:37:35 -0400

    remove image axios request

--- client/src/components/sidebars/RightSidebar.js
+++ client/src/components/sidebars/RightSidebar.js
@@ -1,32 +1,6 @@
 import React from "react";
-import axios from "axios";
 
 class RightSidebar extends React.Component {
-  constructor(props) {
-    super(props);
-
-    this.getImage = this.getImage.bind(this);
-    this.parseData = this.parseData.bind(this);
-  }
-
-  parseData(data) {
-    let str = data.reduce(function (a, b) { return a + String.fromCharCode(b) }, '');
-    return btoa(str).replace(/.{76}(?=.)/g, '$&\n');
-  }
-
-  getImage() {
-    axios.get(`http://localhost:5000/image`, {
-      headers: {
-        "Access-Control-Allow-Origin": "*"
-      }
-    }).then(response => {
-      console.log(response);
-      this.props.saveImageString(this.parseData(response.data))
-    }).catch(error => {
-      console.log(error);
-    });
-  }
-
   render() {
     return (
       <div className="right-sidebar">
@@ -45,7 +19,6 @@ class RightSidebar extends React.Component {
             style={{ backgroundColor: "rgb(243, 240, 233)" }}
             onClick={e => this.props.updateBackgroundColor("rgb(243, 240, 233)")}></button>
         </ul>
-      <button onClick={this.getImage}>Image Get Test</button>
       </div>
     )
   }
