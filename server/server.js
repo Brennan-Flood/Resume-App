aefb877df02807d717346a2f4579fcb8cc0d2c8b
Brennan <flood.brennan@gmail.com>
2020-05-27 02:52:47 -0400

    prepare for production

--- server/server.js
+++ server/server.js
@@ -8,13 +8,13 @@ const expressGraphQL = require("express-graphql");
 const path = require('path');
 const schema = require("./schema/schema");
 
-if (process.env.NODE_ENV === "production") {
+// if (process.env.NODE_ENV === "production") {
   app.use(express.static('client/build'));
 
   app.get('/', (req, res) => {
     res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
   })
-}
+// }
 
 
 
